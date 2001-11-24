0db7967797261f4f5d2e6154753d99f15b40232d
adam <adam>
2001-11-24 01:42:40 +0000

    *** empty log message ***

--- bin/on-cvs-host.pl
+++ bin/on-cvs-host.pl
@@ -3,10 +3,8 @@ die "no .my-cvs-host\n" unless $cvs_host;
 
 (my $me = $0) =~ s!$ENV{HOME}/!!;
 if (hostfqdn() !~ $cvs_host) {
-  my $cmd = "ssh $cvs_host $me";
-  $cmd .= ' ' . join(' ', map { qq{"$_"} } @ARGV) if @ARGV;
-#  warn "$cmd\n";
-  exec $cmd;
+  warn "ssh $cvs_host $me\n";
+  exec "ssh $cvs_host $me";
 }
 
 1;
