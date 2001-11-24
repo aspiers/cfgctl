3bfc9dd7e9bd1984e85b66552cf3efbe21279349
adam <adam>
2001-11-24 01:43:42 +0000

    oops

--- bin/on-cvs-host.pl
+++ bin/on-cvs-host.pl
@@ -4,7 +4,7 @@ die "no .my-cvs-host\n" unless $cvs_host;
 (my $me = $0) =~ s!$ENV{HOME}/!!;
 if (hostfqdn() !~ $cvs_host) {
   warn "ssh $cvs_host $me\n";
-  exec "ssh $cvs_host $me ", join(' ', map { qq{"$_"} } @ARGV;
+  exec "ssh $cvs_host $me ", join(' ', map { qq{"$_"} } @ARGV);
 }
 
 1;
