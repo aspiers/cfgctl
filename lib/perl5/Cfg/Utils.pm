3ba966a1b5abd269457cd1d76368847fb0a3e604
adam <adam>
2006-05-11 20:57:20 +0000

    document preempt_conflict and improve var naming

--- lib/perl5/Cfg/Utils.pm
+++ lib/perl5/Cfg/Utils.pm
@@ -49,64 +49,91 @@ sub ensure_correct_symlink {
   }
 }
 
+=head2 preempt_conflict($src, $dst)
+
+The arguments are full paths to pairs of files which conflict.
+
+If we know in advance via a dry run of stow that a real run would
+result in conflicts (where files in the target tree already exist and
+are not controlled by stow so cannot safely be removed), this routine
+preempts the conflict in one of two ways:
+
+=over 4
+
+=item * Source and destination already have identical contents
+
+In this case, the destination file can be unlinked.
+
+=item * Source and destination have different contents
+
+In this case, we move the destination file over the source so that
+stow can put a symlink there instead whilst still preserving local
+changes.  We take a backup of the original source file first, in case
+we are offline and can't easily retrieve it from the source control
+system.
+
+=back
+
+=cut
+
 sub preempt_conflict {
-  my ($src, $symlink) = @_;
+  my ($src, $dst) = @_;
 
-  debug("preempting conflict between $src and $symlink\n");
+  debug("preempting conflict between $src and $dst\n");
 
-  # Shorter, human-readable versions of source file and symlink
-  (my $human_src     = $src)     =~ s!^$ENV{HOME}/!~/!;
-  (my $human_symlink = $symlink) =~ s!^$ENV{HOME}/!~/!;
-  (my $sub_symlink   = $symlink) =~ s!^$cfg{TARGET_DIR}/!!
-    or die "$symlink didn't start with $cfg{TARGET_DIR}\n";
+  # Shorter, human-readable versions of source and destination
+  (my $human_src = $src) =~ s!^$ENV{HOME}/!~/!;
+  (my $human_dst = $dst) =~ s!^$ENV{HOME}/!~/!;
+  (my $sub_dst   = $dst) =~ s!^$cfg{TARGET_DIR}/!!
+    or die "$dst didn't start with $cfg{TARGET_DIR}\n";
 
-  return if -d $symlink and -d $src;
+  return if -d $dst and -d $src;
 
-  die "$human_symlink is a directory but $human_src isn't; aborting!\n"
-    if -d $symlink and ! -d $src;
+  die "$human_dst is a directory but $human_src isn't; aborting!\n"
+    if -d $dst and ! -d $src;
 
-  if (! -d $symlink and ! -l $symlink and -d $src) {
-    die "$human_src is a directory but $human_symlink isn't; aborting!\n";
+  if (! -d $dst and ! -l $dst and -d $src) {
+    die "$human_src is a directory but $human_dst isn't; aborting!\n";
   }
 
 #   my $tempdir = TempDir->get;
 #   die unless -d $tempdir;
 
-  my ($src_dev,     $src_ino)     = stat($src) or die "stat($src) failed: $!";
-  my ($symlink_dev, $symlink_ino) = stat($symlink);
+  my ($src_dev, $src_ino) = stat($src) or die "stat($src) failed: $!";
+  my ($dst_dev, $dst_ino) = stat($dst);
 
-  if (! $symlink_dev || ! $symlink_ino) {
-    if (-l $symlink) {
+  if (! $dst_dev || ! $dst_ino) {
+    if (-l $dst) {
       if ($opts{'remove-dangling'}) {
-        debug("! Removing dangling symlink $symlink\n");
+        debug("! Removing dangling symlink $dst\n");
         if (for_real()) {
-          unlink($symlink) or die "unlink($symlink) failed: $!\n";
+          unlink($dst) or die "unlink($dst) failed: $!\n";
         }
         return;
       }
       else {
-        die "stat($symlink) failed ($!); invalid symlink?  Specify -r to remove dangling symlinks.\n";
+        die "stat($dst) failed ($!); invalid symlink?  Specify -r to remove dangling symlinks.\n";
       }
     }
     else {
-      die "stat($symlink) failed ($!); can't preempt conflict!  Aborting.\n";
+      die "stat($dst) failed ($!); can't preempt conflict!  Aborting.\n";
     }
   }
 
-  if ($src_dev == $symlink_dev and $src_ino == $symlink_ino) {
-    debug("$human_src and $human_symlink are the same file; must be false conflict (see 6.3 Conflicts section of manual)\n");
+  if ($src_dev == $dst_dev and $src_ino == $dst_ino) {
+    debug("$human_src and $human_dst are the same file; must be false conflict (see 6.3 Conflicts section of manual)\n");
     return;
   }
 
-  if (compare($src, $symlink) == 0) {
+  if (compare($src, $dst) == 0) {
     # same file contents - remove target so that stow can put a
     # symlink there instead.
-    debug("# $human_symlink == $src; remove to make way for symlink\n");
+    debug("# $human_dst == $src; remove to make way for symlink\n");
 #     my $same_dir = File::Spec->join($tempdir, 'same');
 #     die $same_dir unless -d $same_dir;
     if (for_real()) {
-      unlink $symlink or die "unlink($symlink) to preempt conflict failed: $!\n";
-      #move_with_subpath($cfg{TARGET_DIR}, $same_dir, $sub_symlink);
+      unlink $dst or die "unlink($dst) to preempt conflict failed: $!\n";
+      #move_with_subpath($cfg{TARGET_DIR}, $same_dir, $sub_dst);
     }
   }
   else {
@@ -114,17 +141,17 @@ sub preempt_conflict {
     # can put a symlink there instead whilst still preserving
     # local changes
 #     my $modified_dir = File::Spec->join($tempdir, 'modified');
-    print "M $human_symlink\n";
+    print "M $human_dst\n";
     if (for_real()) {
       my $host = $ENV{HOST} || $ENV{HOSTNAME} || die "edge case";
       my $suffix = "cfgsave.$host.$$." . time();
       rename $src, "$src.$suffix"
         or die "rename($src, $src.$suffix) failed: $!\n";
       debug("mv pristine $human_src => $human_src.$suffix\n");
-      rename $symlink, $src
-	or die "rename($symlink, $src) to preempt conflict failed: $!\n";
-      debug("mv modified $human_symlink => $human_src\n");
-      #move_with_subpath($cfg{TARGET_DIR}, $modified_dir, $sub_symlink);
+      rename $dst, $src
+	or die "rename($dst, $src) to preempt conflict failed: $!\n";
+      debug("mv modified $human_dst => $human_src\n");
+      #move_with_subpath($cfg{TARGET_DIR}, $modified_dir, $sub_dst);
     }
   }
 }
